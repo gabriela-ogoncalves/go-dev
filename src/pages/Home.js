1a4638c8ecc9916bbc92a74ada169c3a07fdf9e0
Gabriela Gonçalves <gabrielaog2@yahoo.com.br>
2022-11-20 22:29:57 -0300

    create banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import Banner from '../components/Banner/Banner';
 import TrilhasGrid from '../components/Grid/Grid';
 
 const Home = () => {
   return (
     <div data-testid='home'>
-      <h1>HOME</h1>
+      {/* <h1>HOME</h1> */}
+      <Banner />
       <TrilhasGrid />
       <h1>HOME</h1>
       <h1>HOME</h1>
