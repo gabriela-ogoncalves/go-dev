33bf9576eb36dcf9258d4e508200a38554df7046
Gabriela Gonçalves <gabrielaog2@yahoo.com.br>
2022-11-25 02:40:32 -0300

    create separator component

---
+++ src/components/Separator/Separator.js
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+const Separator = () => {
+  return(
+    <div
+      role='separator'
+      className='separator div-transparent'
+    />
+  );
+};
+
+export default Separator;
