e5783ec9d7137318ae5522a9d38e05251fc02b6a
Gabriela Gonçalves <gabrielaog2@yahoo.com.br>
2022-12-04 01:39:33 -0300

    add getStatus function to utils.js

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,1 +1,10 @@
 export const isMobile = window.innerWidth <= 600;
+
+export const getStatus = (status) => {
+  if (status) {
+    if (status === 'progress') return 'EM ANDAMENTO';
+    if (status === 'done') return 'CONCLUÍDA';  
+  }
+
+  return 'NÃO REALIZADA';
+};
