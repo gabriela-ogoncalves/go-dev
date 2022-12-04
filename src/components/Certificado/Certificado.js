92570465a129522d62cb52989d76f8773c9d0672
Gabriela Gonçalves <gabrielaog2@yahoo.com.br>
2022-12-04 01:40:42 -0300

    adjust certificadoItem to receive props value

--- src/components/Certificado/Certificado.js
+++ src/components/Certificado/Certificado.js
@@ -5,6 +5,8 @@ import signature from '../../assets/images/signature.png';
 import './styles.scss';
 
 const CertificadoItem = React.forwardRef((props, ref) => {
+  const {name, course, workload, date} = props;
+
   return (
     <section ref={ref} data-testid='certificado-item' id='certificado-item'>
        <div className='item'>
@@ -12,9 +14,9 @@ const CertificadoItem = React.forwardRef((props, ref) => {
               <div className='logo'><img src={logo} alt='logo do go dev' /></div>
               <p className='marquee'>Certificado de Conclusão</p>
               <p className='assignment'>Certificamos que o(a) aluno(a)</p>
-              <p className='person'>Nome do aluno</p>
+              <p className='person'>{name || 'Nome do aluno'}</p>
               <p className='reason'>
-                concluiu o curso de <b>Nome do curso</b>, com a carga horária de XX horas, no dia <b>xx</b> de <b>xxxxxxx</b> de <b>20xx</b> pela aplicação Go Dev.
+                concluiu o curso de <b>{course || 'Nome do curso'}</b>, com a carga horária de {workload || 'XX'} horas, no dia <b>{date?.day || 'DD'}</b> de <b>{date?.month || 'MMMMM'}</b> de <b>{date?.year || 'YYYY'}</b> pela aplicação Go Dev.
               </p>
               <div className='signature'>
                 <img src={signature} alt='assinatura do go dev' />
